59aeef40f1a5bd408eb08cd3b60eae00d87b7189
caldotdev <caldotdev@proton.me>
2022-11-07 07:17:15 +0100

    removed unused boilerplate code

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,4 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { ConfiguratorModule } from './configurator/configurator.module';
 
 @Module({
